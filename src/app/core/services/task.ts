0637736f1028c8be79c0dce2e1c6d9641f0e0f75
Robinson Lhuillier <robinson.lhuillier@edu.igensia.com>
2025-11-26 11:38:23 +0100

    count ok

--- src/app/core/services/task.ts
+++ src/app/core/services/task.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root',
 })
 export class Task {
-  
+  private tasks = [
+    { id: 1, title: 'Préparer le cours angular'},
+    { id: 2, title: 'Relire le module RxJs'},
+    { id: 3, title: 'Corriger les TP\'s'}
+  ];
+
+  getTasks() {
+    return of(this.tasks).pipe(delay(1000))
+  }
 }
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common'
+import { Task } from '../core/services/task';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
+  tasks$!: ReturnType<Task['getTasks']>;
+  count: number;
 
+  constructor(private taskService: Task) {
+    this.tasks$ = this.taskService.getTasks();
+    this.count = 0;
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit exécuté')
+    setInterval(() => {
+      this.count++;
+    }, 500);
+  }
 }
