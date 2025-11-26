ef6bb6afd669652d086f5100329ad1f28e956916
Robinson Lhuillier <robinson.lhuillier@edu.igensia.com>
2025-11-26 10:58:52 +0100

    first routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: Home
+    },
+    {
+        path: 'about', component: About
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home'
+import { About } from './about/about'
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, About, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+
 export class App {
   protected readonly title = signal('TaskBoard_Pro');
 }
